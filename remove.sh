9a8f559d07e0712f23b5bee38af00c558609f609
JP <qkrwjdrb@gmail.com>
2018-04-08 19:25:44 -0400

    merging Disha's code

---
+++ remove.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/usr/bin/hadoop fs -rm -r -f open_string_metadata.csv
+/usr/bin/hadoop fs -rm -r -f open_time_metadata.csv
+/usr/bin/hadoop fs -rm -r -f open_num_metadata.csv
+/usr/bin/hadoop fs -rm -r -f parking_string_metadata.csv
+/usr/bin/hadoop fs -rm -r -f parking_time_metadata.csv
+/usr/bin/hadoop fs -rm -r -f parking_num_metadata.csv
