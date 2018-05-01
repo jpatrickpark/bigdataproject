851aedb0d731c6b8c550af38f45bfb69f168c48f
JP <qkrwjdrb@gmail.com>
2018-05-01 15:47:12 -0400

    commented out remove script since dangerous

--- remove.sh
+++ remove.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/bin/hadoop fs -rm -r -f *_string_metadata.csv
-/usr/bin/hadoop fs -rm -r -f *_time_metadata.csv
-/usr/bin/hadoop fs -rm -r -f *_num_metadata.csv
+#/usr/bin/hadoop fs -rm -r -f *_string_metadata.csv
+#/usr/bin/hadoop fs -rm -r -f *_time_metadata.csv
+#/usr/bin/hadoop fs -rm -r -f *_num_metadata.csv
