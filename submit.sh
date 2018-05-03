b19f08f9ff0a1e169c199ee505c8bca49b8ae998
Disha Umarwani <dhu200@nyu.edu>
2018-05-03 16:24:46 -0400

    Delete submit.sh

--- submit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-spark-submit --conf spark.pyspark.python=/share/apps/python/3.4.4/bin/python example.py /user/ecc290/HW1data/parking-violations-header.csv /user/ecc290/HW1data/open-violations-header.csv
